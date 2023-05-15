4cdd43858d89b90dcb6eade393a127adccb31804
Tânia Guedes <93449169+TaniaGuedes5@users.noreply.github.com>
2023-05-15 19:00:06 +0100

    .

---
+++ tic-tac-toe-game/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import React, { useState } from "react";
+
+function App() {
+
+ 
+  return (
+    <div className="App">
+      <div className="board">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
