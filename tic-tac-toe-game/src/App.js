9b5de1f41bb2b4162e661b617c1f816bab1a29e1
Tânia Guedes <93449169+TaniaGuedes5@users.noreply.github.com>
2023-05-24 17:48:19 +0100

    mini tab

--- tic-tac-toe-game/src/App.js
+++ tic-tac-toe-game/src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 
 import React, { useState } from "react";
 
+import { Board } from './componentes/Board';
+
 function App() {
-
+  
+  const [board, setBoard] =  useState(Array(9).fill(null));
  
+  const handleBoxClick = (idBox) => {
+    //Passo 1: atualizar tabuleiro
+    //Passo2: verificar se alguem ganhou o jogo
+    //Passo3: mudar de jogador
+  }
   return (
-    <div className="App">
-      <div className="board">
 
+    <div className="App">
+      <div className='board'>
+        <Board board={board} onClick={null}/>
       </div>
     </div>
   );
