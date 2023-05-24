350b46a44bf9dc947d114845d6ed07111830c619
Tânia Guedes <93449169+TaniaGuedes5@users.noreply.github.com>
2023-05-24 17:52:56 +0100

    passo 1 in app.js

--- tic-tac-toe-game/src/App.js
+++ tic-tac-toe-game/src/App.js
@@ -10,7 +10,18 @@ function App() {
  
   const handleBoxClick = (idBox) => {
     //Passo 1: atualizar tabuleiro
+    const updatedBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
+        return xPlaying ? "X" : "O";
+      } else {
+        return value;
+      }
+    })
+    setBoard(updateBoard);
+
     //Passo2: verificar se alguem ganhou o jogo
+
+    
     //Passo3: mudar de jogador
   }
   return (
