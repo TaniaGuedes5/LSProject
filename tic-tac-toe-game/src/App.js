d6b5139bb798743374ccb942fe66ec8cffb177d2
Tânia Guedes <93449169+TaniaGuedes5@users.noreply.github.com>
2023-05-24 18:32:45 +0100

    trying step 2in app.js

--- tic-tac-toe-game/src/App.js
+++ tic-tac-toe-game/src/App.js
@@ -7,22 +7,24 @@ import { Board } from './componentes/Board';
 function App() {
   
   const [board, setBoard] =  useState(Array(9).fill(null));
- 
+  const [player, setPlayer] = useState(true);
+
   const handleBoxClick = (idBox) => {
     //Passo 1: atualizar tabuleiro
-    const updatedBoard = board.map((value, idx) => {
-      if (idx === boxIdx) {
-        return xPlaying ? "X" : "O";
+    const updatedBoard = board.map((value, id) => {
+      if (id === idBox) {
+        return player ? "X" : "O";
       } else {
         return value;
       }
     })
-    setBoard(updateBoard);
+    setBoard(updatedBoard);
 
     //Passo2: verificar se alguem ganhou o jogo
-
-    
+    const winner = checkWinner(updatedBoard);
+ 
     //Passo3: mudar de jogador
+    setPlayer(!player);
   }
   return (
 
