7816b57bc0d362c1e96d2b9de9a90f91be48bdaa
therxlr <60614365+therxlr@users.noreply.github.com>
2023-06-14 15:26:54 +0100

    Add files via upload

--- RodrigoChanges/LSProject/tic-tac-toe-game/src/componentes/GameBoard/BigBoard.js
+++ RodrigoChanges/LSProject/tic-tac-toe-game/src/componentes/GameBoard/BigBoard.js
@@ -1,16 +1,31 @@
 import React from "react";
 
-import "./BigBoard.css"
-import { Board } from "./Board"
+import "./BigBoard.css";
+import { Board } from "./Board";
 
-export const BigBoard = ({ boards, onCellClick }) => {
+export const BigBoard = ({ boards, lockedBoards, smallBoardWinners, onCellClick }) => {
     return (
-      <div className="big-board">
-        {boards.map((board, boardIndex) => (
-          <div key={boardIndex} className="mini-board">
-            <Board board={board} onClick={(cellIndex) => onCellClick(boardIndex, cellIndex)} />
-          </div>
-        ))}
-      </div>
+        <div className="big-board">
+            {boards.map((board, boardIndex) => (
+                <div key={boardIndex} className="mini-board">
+                    <Board
+                        board={board}
+                        isLocked={lockedBoards[boardIndex]}
+                        onClick={(cellIndex) => onCellClick(boardIndex, cellIndex)}
+                    />
+                    {lockedBoards[boardIndex] && (
+                        <div className="lock-overlay">
+                            {smallBoardWinners[boardIndex] === 'X' ? (
+                                <div className="cross"></div>
+                            ) : smallBoardWinners[boardIndex] === 'O' ? (
+                                <div className="circle"></div>
+                            ) : smallBoardWinners[boardIndex] === 'D' ? (
+                                <div className="draw-dash"></div>
+                            ) : null}
+                        </div>
+                    )}
+                </div>
+            ))}
+        </div>
     );
-  };
+};
--- RodrigoChanges/LSProject/tic-tac-toe-game/src/componentes/GameBoard/Board.js
+++ RodrigoChanges/LSProject/tic-tac-toe-game/src/componentes/GameBoard/Board.js
@@ -4,14 +4,14 @@ import React from 'react';
 import { Box } from "./Box"
 import "./Board.css"
 
-export const Board = ({ board, onClick }) => {
+export const Board = ({ board, isLocked, onClick }) => {
     return (
-      <div className="board">
-        {
-          board.map((value, idx) => {
-            return <Box value={value} onClick={() => value === null && onClick(idx)} />;
-          })
-        }
-      </div>
+        <div className="board">
+            {
+                board.map((value, idx) => {
+                    return <Box value={value} onClick={() => !isLocked && value === null && onClick(idx)} />;
+                })
+            }
+        </div>
     )
-  }
+}
